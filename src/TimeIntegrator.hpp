4a32224e5f879f5ecc41bfaf7299936397fab889
gidon.bauer <gidon.bauer@hotmail.de>
2025-12-02 23:22:42 +0100

    Create common base class for all time integrators

--- src/TimeIntegrator.hpp
+++ src/TimeIntegrator.hpp
@@ -8,36 +8,26 @@ namespace UPS {
 
 // =================================================================================================
 template <typename RHS, typename BCond, typename AdjustTimestep>
-class ExplicitEuler {
+class TimeIntegrator {
+ protected:
   RHS rhs;
   BCond bcond;
   AdjustTimestep adjust_timestep;
   Grid grid;
 
-  Vector<double> dudt;
-
  public:
   Vector<double> u;
 
- private:
-  constexpr auto do_step(double dt) noexcept -> double {
-    rhs(grid, u, dudt);
-    for (Index i = 0; i < u.extent(); ++i) {
-      u[i] += dt * dudt[i];
-    }
-    bcond(u);
-
-    return dt;
-  }
+ protected:
+  virtual constexpr auto do_step(double dt) noexcept -> double = 0;
 
  public:
-  constexpr ExplicitEuler(
+  constexpr TimeIntegrator(
       Grid grid, RHS rhs, BCond bcond, AdjustTimestep adjust_timestep, const Vector<double>& u0)
       : rhs(std::move(rhs)),
         bcond(std::move(bcond)),
         adjust_timestep(std::move(adjust_timestep)),
         grid(std::move(grid)),
-        dudt(grid.N, grid.NGhost),
         u(u0) {
     IGOR_ASSERT(grid.N == u0.extent(), "Incompatible size of grid and u0");
     IGOR_ASSERT(grid.NGhost == u0.nghost(), "Incompatible size of grid and u0");
@@ -57,19 +47,52 @@ class ExplicitEuler {
 
 // =================================================================================================
 template <typename RHS, typename BCond, typename AdjustTimestep>
-class RungeKutta2 {
-  RHS rhs;
-  BCond bcond;
-  AdjustTimestep adjust_timestep;
-  Grid grid;
-
-  Vector<double> dudt;
+class ExplicitEuler final : public TimeIntegrator<RHS, BCond, AdjustTimestep> {
+  using TI = TimeIntegrator<RHS, BCond, AdjustTimestep>;
+  using TI::bcond;
+  using TI::grid;
+  using TI::rhs;
 
  public:
-  Vector<double> u;
+  using TI::solve;
+  using TI::u;
 
  private:
-  constexpr auto do_step(double dt) noexcept -> double {
+  Vector<double> dudt;
+
+  constexpr auto do_step(double dt) noexcept -> double override {
+    rhs(grid, u, dudt);
+    for (Index i = 0; i < u.extent(); ++i) {
+      u[i] += dt * dudt[i];
+    }
+    bcond(u);
+
+    return dt;
+  }
+
+ public:
+  constexpr ExplicitEuler(
+      Grid grid, RHS rhs, BCond bcond, AdjustTimestep adjust_timestep, const Vector<double>& u0)
+      : TI(std::move(grid), std::move(rhs), std::move(bcond), std::move(adjust_timestep), u0),
+        dudt(u0.extent(), u0.nghost()) {}
+};
+
+// =================================================================================================
+template <typename RHS, typename BCond, typename AdjustTimestep>
+class RungeKutta2 final : public TimeIntegrator<RHS, BCond, AdjustTimestep> {
+  using TI = TimeIntegrator<RHS, BCond, AdjustTimestep>;
+  using TI::bcond;
+  using TI::grid;
+  using TI::rhs;
+
+ public:
+  using TI::solve;
+  using TI::u;
+
+ private:
+  Vector<double> dudt;
+
+  constexpr auto do_step(double dt) noexcept -> double override {
     for (Index step = 0; step < 2; ++step) {
       rhs(grid, u, dudt);
       for (Index i = 0; i < u.extent(); ++i) {
@@ -84,45 +107,28 @@ class RungeKutta2 {
  public:
   constexpr RungeKutta2(
       Grid grid, RHS rhs, BCond bcond, AdjustTimestep adjust_timestep, const Vector<double>& u0)
-      : rhs(std::move(rhs)),
-        bcond(std::move(bcond)),
-        adjust_timestep(std::move(adjust_timestep)),
-        grid(std::move(grid)),
-        dudt(grid.N, grid.NGhost),
-        u(u0) {
-    IGOR_ASSERT(grid.N == u0.extent(), "Incompatible size of grid and u0");
-    IGOR_ASSERT(grid.NGhost == u0.nghost(), "Incompatible size of grid and u0");
-  }
-
-  constexpr void solve(double tend) noexcept {
-    double t  = 0.0;
-    double dt = adjust_timestep(grid, u);
-    while (t < tend) {
-      dt  = adjust_timestep(grid, u);
-      dt  = std::min(dt, tend - t);
-      dt  = do_step(dt);
-      t  += dt;
-    }
-  }
+      : TI(std::move(grid), std::move(rhs), std::move(bcond), std::move(adjust_timestep), u0),
+        dudt(u0.extent(), u0.nghost()) {}
 };
 
 // =================================================================================================
 template <typename RHS, typename BCond, typename AdjustTimestep>
-class SemiImplicitCrankNicolson {
-  RHS rhs;
-  BCond bcond;
-  AdjustTimestep adjust_timestep;
-  Grid grid;
+class SemiImplicitCrankNicolson final : public TimeIntegrator<RHS, BCond, AdjustTimestep> {
+  using TI = TimeIntegrator<RHS, BCond, AdjustTimestep>;
+  using TI::bcond;
+  using TI::grid;
+  using TI::rhs;
 
+ public:
+  using TI::solve;
+  using TI::u;
+
+ private:
   Index num_subiter;
   Vector<double> dudt;
   Vector<double> u_old;
 
- public:
-  Vector<double> u;
-
- private:
-  constexpr auto do_step(double dt) noexcept -> double {
+  constexpr auto do_step(double dt) noexcept -> double override {
     std::copy_n(u.data(), u.size(), u_old.data());
 
     for (Index sub_iter = 0; sub_iter < num_subiter; ++sub_iter) {
@@ -151,29 +157,12 @@ class SemiImplicitCrankNicolson {
                                       AdjustTimestep adjust_timestep,
                                       const Vector<double>& u0,
                                       Index num_subiter = 5)
-      : rhs(std::move(rhs)),
-        bcond(std::move(bcond)),
-        adjust_timestep(std::move(adjust_timestep)),
-        grid(std::move(grid)),
+      : TI(std::move(grid), std::move(rhs), std::move(bcond), std::move(adjust_timestep), u0),
         num_subiter(num_subiter),
-        dudt(grid.N, grid.NGhost),
-        u_old(u0),
-        u(u0) {
-    IGOR_ASSERT(grid.N == u0.extent(), "Incompatible size of grid and u0");
-    IGOR_ASSERT(grid.NGhost == u0.nghost(), "Incompatible size of grid and u0");
+        dudt(u0.extent(), u0.nghost()),
+        u_old(u0.extent(), u0.nghost()) {
     IGOR_ASSERT(
         num_subiter > 0, "Number of sub-iterations must be greater than 0 but is {}", num_subiter);
-  }
-
-  constexpr void solve(double tend) noexcept {
-    double t  = 0.0;
-    double dt = adjust_timestep(grid, u);
-    while (t < tend) {
-      dt  = adjust_timestep(grid, u);
-      dt  = std::min(dt, tend - t);
-      dt  = do_step(dt);
-      t  += dt;
-    }
   }
 };
 
